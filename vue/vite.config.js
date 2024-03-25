8839e2f7f8bb6a9d480712a31484c434835799f7
S1m5lar <107901928+S1m5lar@users.noreply.github.com>
2024-03-25 17:11:54 +0800

    Create vite.config.js

---
+++ vue/vite.config.js
@@ -0,0 +1,48 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+// 导入对应包
+
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
+import ElementPlus from 'unplugin-element-plus/vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver(
+          { importStyle: 'sass' }
+      )],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver(
+          { importStyle: 'sass' }
+      )],
+    }),
+
+    // 按需定制主题配置
+    ElementPlus({
+      useSource: true,
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // 自动导入定制化样式文件进行样式覆盖
+        additionalData: `
+          @use "@/assets/css/index.scss" as *;
+        `,
+      }
+    }
+  }
+})
